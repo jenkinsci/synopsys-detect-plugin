777b6b51877b9ad0c8382d4eabb0d09834f80c00
jrichard <jrichard@blackducksoftware.com>
2017-08-29 10:47:49 -0400

    Adding correct headers

--- src/main/java/com/blackducksoftware/integration/detect/jenkins/exception/DetectJenkinsException.java
+++ src/main/java/com/blackducksoftware/integration/detect/jenkins/exception/DetectJenkinsException.java
@@ -1,5 +1,7 @@
-/*******************************************************************************
- * Copyright (C) 2016 Black Duck Software, Inc.
+/**
+ * Black Duck Detect Plugin for Jenkins
+ *
+ * Copyright (C) 2017 Black Duck Software, Inc.
  * http://www.blackducksoftware.com/
  *
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -18,7 +20,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *******************************************************************************/
+ */
 package com.blackducksoftware.integration.detect.jenkins.exception;
 
 public class DetectJenkinsException extends Exception {
