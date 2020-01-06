81128d398ed27a349253be22c017be7b28dc3de3
Steve Billings <sbillings@blackducksoftware.com>
2020-01-06 18:43:11 -0500

    test: DetectPostBuildStepTest: fixed fragility

--- src/test/java/com/synopsys/integration/jenkins/detect/extensions/postbuild/DetectPostBuildStepTest.java
+++ src/test/java/com/synopsys/integration/jenkins/detect/extensions/postbuild/DetectPostBuildStepTest.java
@@ -2,12 +2,10 @@ package com.synopsys.integration.jenkins.detect.extensions.postbuild;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.powermock.configuration.ConfigurationType.PowerMock;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.recipes.WithPlugin;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import org.powermock.api.mockito.PowerMockito;
@@ -32,14 +30,11 @@ import hudson.FilePath;
 import hudson.Launcher;
 import hudson.PluginManager;
 import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
-import hudson.model.Build;
 import hudson.model.BuildListener;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.JDK;
 import hudson.model.Node;
-import hudson.model.Project;
 import hudson.model.Result;
 import hudson.model.TaskListener;
 import hudson.remoting.VirtualChannel;
@@ -55,7 +50,6 @@ public class DetectPostBuildStepTest {
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
-    //fails: @WithPlugin({"credentials", "plain-credentials"})
     @Ignore
     @Test
     public void test() throws Exception {
@@ -87,12 +81,10 @@ public class DetectPostBuildStepTest {
         System.out.println("Done!!");
     }
 
-    //@Ignore
     @Test
     public void testPerform() throws Exception {
         System.out.println("Starting testPerform()!");
         final DetectPostBuildStep detectPostBuildStep = new DetectPostBuildStep(DETECT_PROPERTY_INPUT);
-        // TODO make build a FreeStyleBuild
         final AbstractBuild<FreeStyleProject, FreeStyleBuild> build = PowerMockito.mock(AbstractBuild.class);
         final Launcher launcher = PowerMockito.mock(Launcher.class);
 
@@ -125,10 +117,7 @@ public class DetectPostBuildStepTest {
         Mockito.when(launcher.getChannel()).thenReturn(channel);
         final DetectSetupResponse detectSetupResponse = PowerMockito.mock(DetectSetupResponse.class);
         Mockito.when(channel.call(Mockito.any(SetUpDetectWorkspaceCallable.class))).thenReturn(detectSetupResponse);
-
-
         Mockito.when(detectSetupResponse.getExecutionStrategy()).thenReturn(DetectSetupResponse.ExecutionStrategy.JAR);
-
         Mockito.when(detectSetupResponse.getDetectRemotePath()).thenReturn("/tmp/detect.jar");
 
         // This getHome() method actually returns the path to the java exe
@@ -144,13 +133,25 @@ public class DetectPostBuildStepTest {
         Mockito.when(procStarter.join()).thenReturn(0);
 
         final boolean succeeded = detectPostBuildStep.perform(build, launcher, listener);
+
         assertTrue(succeeded);
         Mockito.verify(build, Mockito.never()).setResult(Result.ABORTED);
         Mockito.verify(build, Mockito.never()).setResult(Result.UNSTABLE);
 
-        final List<String> expectedCmds = Arrays.asList("/tmp/jdk/bin/java", "-jar", "/tmp/detect.jar", "--detect.docker.passthrough.service.timeout=240000", "--detect.cleanup=false",
-            "--logging.level.com.synopsys.integration=INFO", "--detect.phone.home.passthrough.jenkins.version=2.138.4", "--detect.phone.home.passthrough.jenkins.plugin.version=2.1.1-SNAPSHOT");
-        Mockito.verify(procStarter).cmds(expectedCmds);
+        ArgumentCaptor<List<String>> cmdsArgCapture = ArgumentCaptor.forClass(List.class);
+        Mockito.verify(procStarter).cmds(cmdsArgCapture.capture());
+        final List<String> actualCmds = cmdsArgCapture.getValue();
+        System.out.printf("actualCmds: %s\n", actualCmds);
+        int i=0;
+        assertEquals("/tmp/jdk/bin/java", actualCmds.get(i++));
+        assertEquals("-jar", actualCmds.get(i++));
+        assertEquals("/tmp/detect.jar", actualCmds.get(i++));
+        assertEquals("--detect.docker.passthrough.service.timeout=240000", actualCmds.get(i++));
+        assertEquals("--detect.cleanup=false", actualCmds.get(i++));
+        assertEquals("--logging.level.com.synopsys.integration=INFO", actualCmds.get(i++));
+        assertTrue(actualCmds.get(i++).startsWith("--detect.phone.home.passthrough.jenkins.version="));
+        assertTrue(actualCmds.get(i++).startsWith("--detect.phone.home.passthrough.jenkins.plugin.version="));
+
         System.out.println("Done!!");
     }
 }
