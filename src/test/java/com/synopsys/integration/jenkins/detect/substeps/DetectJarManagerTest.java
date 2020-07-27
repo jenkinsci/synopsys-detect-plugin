e42f83da21e1e48d5b5b37e8f286c3075ec194f6
Dana Maxfield <danam@synopsys.com>
2020-07-27 15:11:59 -0400

    test: Break into individual tests VS parameterized tests (IDTCTJNKNS-201)

--- src/test/java/com/synopsys/integration/jenkins/detect/substeps/DetectJarManagerTest.java
+++ src/test/java/com/synopsys/integration/jenkins/detect/substeps/DetectJarManagerTest.java
@@ -9,11 +9,9 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.synopsys.integration.jenkins.extensions.JenkinsIntLogger;
@@ -23,70 +21,115 @@ import hudson.model.TaskListener;
 
 public class DetectJarManagerTest {
 
-    private final static String testDetectJarPath = "/test/detect/jar/path/detect.jar";
-    private final static String testJavaHome = "/test/java/home/path";
+    private final static String detectJarPathInput = "/test/detect/jar/path/detect.jar";
+    private final static String javaHomeInput = "/test/java/home/path";
     private final static String javaBinPath = "/bin/java";
-    private final static String expectedJavaHome = testJavaHome + javaBinPath;
-    private final static String testPathEnv = "/test/path/env";
+    private final static String javaPath = javaHomeInput + javaBinPath;
+    private final static String javaPathSystem = System.getProperty("user.dir") + "/ " + javaBinPath;
+    private final static String pathEnvVarInput = "/test/path/env";
+
+    private final static Map<String, String> environmentVariables = new HashMap<>();
+
+    private final TaskListener taskListener = Mockito.mock(TaskListener.class);
+    private final JenkinsIntLogger jenkinsIntLogger = new JenkinsIntLogger(taskListener);
 
     private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-    private static Stream<Arguments> setUpForExecutionInput() {
-        return Stream.of(
-            Arguments.of(null, "java", true, null, testPathEnv, LogLevel.DEBUG),
-            Arguments.of(testJavaHome, expectedJavaHome, false, testDetectJarPath, testPathEnv, LogLevel.WARN),
-            Arguments.of(testJavaHome, expectedJavaHome, false, testDetectJarPath, testPathEnv, LogLevel.INFO),
-            Arguments.of(testJavaHome, expectedJavaHome, false, testDetectJarPath, testPathEnv, LogLevel.DEBUG),
-            Arguments.of(testJavaHome, expectedJavaHome, false, testDetectJarPath, testPathEnv, LogLevel.TRACE),
-            Arguments.of("", javaBinPath, false, "", "", LogLevel.DEBUG),
-            Arguments.of(" ", System.getProperty("user.dir") + "/ " + javaBinPath, false, " ", " ", LogLevel.DEBUG),
-            Arguments.of("", javaBinPath, false, "", "", null)
-        );
+    @BeforeEach
+    public void setUp() {
+        environmentVariables.put("PATH", pathEnvVarInput);
+        Mockito.when(taskListener.getLogger()).thenReturn(new PrintStream(byteArrayOutputStream));
     }
 
-    @ParameterizedTest
-    @MethodSource({ "setUpForExecutionInput" })
-    public void testSetUpForExecution(String javaHomeInput, String javaHomeExpected, Boolean isValidJavaHome, String detectJarPathInput, String pathEnvInput, LogLevel logLevel) {
-        Map<String, String> environmentVariables = new HashMap<>();
-        environmentVariables.put("PATH", pathEnvInput);
+    @Test
+    public void testSetUpForExecutionEmptySpaceStringInput() {
+        jenkinsIntLogger.setLogLevel(LogLevel.DEBUG);
+        this.createAndTestDetectSetupResponse(" ", " ", javaPathSystem);
+        this.debugLogLevelValidation(javaPathSystem, " ");
+        this.invalidJavaExecValidation();
+    }
 
-        TaskListener taskListener = Mockito.mock(TaskListener.class);
-        Mockito.when(taskListener.getLogger()).thenReturn(new PrintStream(byteArrayOutputStream));
-        JenkinsIntLogger jenkinsIntLogger = new JenkinsIntLogger(taskListener);
+    @Test
+    public void testSetUpForExecutionEmptyStringInput() {
+        jenkinsIntLogger.setLogLevel(LogLevel.DEBUG);
+        this.createAndTestDetectSetupResponse("", "", javaBinPath);
+        this.debugLogLevelValidation(javaBinPath, "");
+        this.invalidJavaExecValidation();
+    }
 
-        if (logLevel != null) {
-            jenkinsIntLogger.setLogLevel(logLevel);
-        }
+    @Test
+    public void testSetUpForExecutionNullInput() {
+        jenkinsIntLogger.setLogLevel(LogLevel.DEBUG);
+        this.createAndTestDetectSetupResponse(null, null, "java");
+        this.debugLogLevelValidation("java", null);
+        this.validJavaExecValidation();
+    }
 
+    @Test
+    public void testSetUpForExecutionLogTrace() {
+        jenkinsIntLogger.setLogLevel(LogLevel.TRACE);
+        this.createAndTestDetectSetupResponse(javaHomeInput, detectJarPathInput, javaPath);
+        this.debugLogLevelValidation(javaPath, detectJarPathInput);
+        this.invalidJavaExecValidation();
+    }
+
+    @Test
+    public void testSetUpForExecutionLogDebug() {
+        jenkinsIntLogger.setLogLevel(LogLevel.DEBUG);
+        this.createAndTestDetectSetupResponse(javaHomeInput, detectJarPathInput, javaPath);
+        this.debugLogLevelValidation(javaPath, detectJarPathInput);
+        this.invalidJavaExecValidation();
+    }
+
+    @Test
+    public void testSetUpForExecutionLogInfo() {
+        jenkinsIntLogger.setLogLevel(LogLevel.INFO);
+        this.createAndTestDetectSetupResponse(javaHomeInput, detectJarPathInput, javaPath);
+        this.infoLogLevelValidation(javaPath, detectJarPathInput);
+    }
+
+    @Test
+    public void testSetUpForExecutionLogWarn() {
+        jenkinsIntLogger.setLogLevel(LogLevel.WARN);
+        this.createAndTestDetectSetupResponse(javaHomeInput, detectJarPathInput, javaPath);
+    }
+
+    @Test
+    public void testSetUpForExecutionLogUnset() {
+        this.createAndTestDetectSetupResponse("", "", javaBinPath);
+        this.infoLogLevelValidation(javaBinPath, "");
+    }
+
+    private void testDetectSetupResponse(DetectSetupResponse detectSetupResponse, String detectJarPathInput, String javaPath) {
+        assertEquals(DetectSetupResponse.ExecutionStrategy.JAR, detectSetupResponse.getExecutionStrategy());
+        assertEquals(detectJarPathInput, detectSetupResponse.getDetectRemotePath());
+        assertEquals(javaPath, detectSetupResponse.getRemoteJavaHome());
+    }
+
+    private void createAndTestDetectSetupResponse(String javaHomeInput, String detectJarPathInput, String expectedJavaPath) {
         try {
-            // Call the method being tested
-            DetectJarManager detectJarManager = new DetectJarManager(jenkinsIntLogger, javaHomeInput, environmentVariables, detectJarPathInput);
-            DetectSetupResponse detectSetupResponse = detectJarManager.setUpForExecution();
-
-            assertEquals(DetectSetupResponse.ExecutionStrategy.JAR, detectSetupResponse.getExecutionStrategy());
-            assertEquals(detectJarPathInput, detectSetupResponse.getDetectRemotePath());
-            assertEquals(javaHomeExpected, detectSetupResponse.getRemoteJavaHome());
-
-            if (jenkinsIntLogger.getLogLevel().isLoggable(LogLevel.INFO)) {
-                assertTrue(byteArrayOutputStream.toString().contains("Running with JAVA: " + javaHomeExpected), "Log does not contain entry for JAVA path.");
-                assertTrue(byteArrayOutputStream.toString().contains("Detect configured: " + detectJarPathInput), "Log does not contain entry for Detect path.");
-            }
-
-            if (jenkinsIntLogger.getLogLevel().isLoggable(LogLevel.DEBUG)) {
-                assertTrue(byteArrayOutputStream.toString().contains("PATH: " + pathEnvInput), "Log does not contain entry for PATH environment variable");
-
-                if (isValidJavaHome) {
-                    assertTrue(byteArrayOutputStream.toString().contains("Java version: \n"), "Log does not contain entry for Java Version heading.");
-                } else {
-                    assertTrue(byteArrayOutputStream.toString().contains("Error printing the JAVA version: "), "Log does not contain error for printing Java version.");
-                }
-
-            }
-
+            testDetectSetupResponse(new DetectJarManager(jenkinsIntLogger, javaHomeInput, environmentVariables, detectJarPathInput).setUpForExecution(), detectJarPathInput, expectedJavaPath);
         } catch (IOException | InterruptedException e) {
             fail("Unexpected exception thrown", e);
         }
+    }
 
+    private void infoLogLevelValidation(String javaPath, String detectJarPathInput) {
+        assertTrue(byteArrayOutputStream.toString().contains("Running with JAVA: " + javaPath), "Log does not contain entry for JAVA path.");
+        assertTrue(byteArrayOutputStream.toString().contains("Detect configured: " + detectJarPathInput), "Log does not contain entry for Detect path.");
+    }
+
+    private void debugLogLevelValidation(String javaPath, String detectJarPathInput) {
+        this.infoLogLevelValidation(javaPath, detectJarPathInput);
+        assertTrue(byteArrayOutputStream.toString().contains("PATH: " + pathEnvVarInput), "Log does not contain entry for PATH environment variable");
+    }
+
+    private void invalidJavaExecValidation() {
+        assertTrue(byteArrayOutputStream.toString().contains("Error printing the JAVA version: "), "Log does not contain error for printing Java version.");
+    }
+
+    private void validJavaExecValidation() {
+        assertTrue(byteArrayOutputStream.toString().contains("Java version: \n"), "Log does not contain entry for Java Version heading.");
     }
 
 }
